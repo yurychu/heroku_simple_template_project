a0e386c91428540e0e49b85498ca00ca646e9389
Yury, oqqrez <yury_chugunov@mail.ru>
2016-09-05 20:41:24 +0300

    templates dir

--- src/configuration/settings.py
+++ src/configuration/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'configuration.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.abspath(os.path.join(BASE_DIR, 'templates/')),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
